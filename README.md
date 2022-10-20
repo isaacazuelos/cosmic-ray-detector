9ee9164c24e397a140ebe45850caf3c7689f4256
Isaac Azuelos <isaac@azuelos.ca>
2022-10-19 21:54:15 -0600

    Add mlock(2), logging and arg parsing

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cosmic-ray-detector
+
 Watches for bit flips in a large buffer.
+
+Use `--help` for details. Writeup incoming, maybe.
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,104 @@
-use ctrlc;
+use std::path::PathBuf;
 
-static mut ERRORS: usize = 0;
-static mut CHECKS: usize = 0;
+use clap::Parser;
+use libc;
+use log::{error, info};
+use simplelog;
 
-fn main() {
-    let size = 8 * 1024 * 1024; // 8 GB
+struct Buffer(Vec<usize>);
 
-    let mut buf = Vec::new();
+impl Buffer {
+    fn new(size_in_gb: usize) -> Buffer {
+        let size_in_bytes = size_in_gb * 1024 * 1024 * 1024;
+        let mut vec = Vec::with_capacity(size_in_bytes / std::mem::size_of::<usize>());
 
-    let start_time = std::time::SystemTime::now();
-
-    print!("initializing...");
-    for i in 0..size {
-        buf.push(i);
-    }
-    println!("done!");
-
-    ctrlc::set_handler(move || unsafe {
-        println!(
-            "A total of {} errors found after checking {} times since {:?}",
-            ERRORS, CHECKS, start_time
-        );
-        std::process::exit(0);
-    })
-    .expect("error setting ctrl-c handler");
-
-    loop {
-        std::thread::sleep(std::time::Duration::from_secs(1));
-
-        for i in 0..buf.len() {
-            if buf[i] != i {
-                println!("error: 0x{:X} was set to 0x{:X}, resetting", i, buf[i]);
-                buf[i] = i;
-                unsafe { ERRORS += 1 };
-            }
+        for i in 0..vec.len() {
+            vec.push(i);
         }
 
-        unsafe {
-            println!("check {}", CHECKS);
-            CHECKS += 1
-        };
+        let mlock_succeeded = unsafe { libc::mlock(vec.as_ptr() as _, size_in_bytes) == 0 };
+        if !mlock_succeeded {
+            error!("error: mlock(2) failed, could not guarantee pages would remain in memory");
+            std::process::exit(1);
+        }
+
+        Buffer(vec)
+    }
+
+    fn scan(&mut self) {
+        for (expected, actual) in self.0.iter().enumerate() {
+            if *actual != expected {
+                info!(
+                    "expected 0b{:b} (i.e. {}) but got 0b{:b}",
+                    expected, expected, actual
+                );
+            }
+        }
+    }
+}
+
+impl Drop for Buffer {
+    fn drop(&mut self) {
+        unsafe { libc::munlockall() };
+    }
+}
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// The size of the buffer to watch, in GB
+    #[arg(short, long, default_value_t = 8)]
+    size: usize,
+
+    /// The delay between iterations, in seconds
+    #[arg(short, long, default_value_t = 1)]
+    delay: u64,
+
+    /// Log the output to a file.
+    #[arg(short, long, value_name = "FILE")]
+    log: Option<PathBuf>,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    // literally the example code
+    {
+        use simplelog::*;
+        use std::fs::File;
+
+        let mut loggers: Vec<Box<dyn SharedLogger>> = vec![TermLogger::new(
+            LevelFilter::Info,
+            Config::default(),
+            TerminalMode::Mixed,
+            ColorChoice::Auto,
+        )];
+
+        if let Some(path) = args.log {
+            loggers.push(WriteLogger::new(
+                LevelFilter::Info,
+                Config::default(),
+                File::create(path).unwrap(),
+            ));
+        }
+
+        CombinedLogger::init(loggers).expect("could not initialize logging");
+    }
+
+    println!("Hit control-c to stop.");
+
+    info!(
+        "running with a {} GB buffer, checking every {} seconds",
+        args.size, args.delay
+    );
+
+    let mut buf = Buffer::new(args.size);
+
+    let mut iteration_count = 0;
+    loop {
+        info!("iteration {}", iteration_count);
+        buf.scan();
+        iteration_count += 1;
+        std::thread::sleep(std::time::Duration::from_secs(args.delay));
     }
 }
